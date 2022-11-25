e8cf46b57cf058146064d534575e5ca93409f80c
Anish-Palakurthi <anish.palakurthi@utexas.edu>
2022-11-25 14:07:33 -0600

    good prog

--- App.js
+++ App.js
@@ -10,24 +10,88 @@ import {
   SafeAreaView,
   Touchable,
   TextInput,
+  ScrollView,
+  ImageBackground,
+  navigation,
+  Pressable
 } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { useNavigation } from '@react-navigation/native';
 
-function LoginScreen() {
+
+function FriendProfile(props) {
+  const id = props.name
   return (
-    <SafeAreaView style={styles.container}>
-      <Image
-        style={{ width: "30%", height: "30%" }}
-        source={require("./assets/AppLogo.png")}
-      />
-      <TextInput style={styles.inputText} placeholder="Username" />
-      <TextInput style={styles.inputText} placeholder="Password" />
-      <Button title="Sign in" color="#E3967E" />
-      <TouchableOpacity>
-        <Text>Forgot password?</Text>
-      </TouchableOpacity>
-      <Button title="New? Sign up" color="#E3967E" />
+    <View>
+      {/* <Image
+        style={styles.friendPic}
+      //source={require(props.name)}
+      /> */}
+      <Text>
+        {JSON.stringify(id)}
+      </Text>
+    </View>
+  );
+
+}
+
+function Friends() {
+  return (
+    <ScrollView>
+      <FriendProfile name="John" />
+      <FriendProfile name="Sarah" />
+      <FriendProfile name="Ellie" />
+      <FriendProfile name="Peter" />
+      <FriendProfile name="Jess" />
+
+    </ScrollView>
+  )
+}
+function LoginScreen() {
+  const navigation = useNavigation();
+  return (
+    <View style={styles.container}>
+      <ImageBackground source={require("./assets/charboard.png")} style={{ flex: 1, width: '100%', height: '100%', opacity: 0.6 }}>
+        <Image
+          style={{
+            flex: 1.1,
+            width: "85%",
+            height: "85%",
+            resizeMode: 'contain',
+            alignSelf: 'center',
+          }}
+          source={require("./assets/AppLogo.png")}
+        />
+
+        <View
+          style={{
+            flex: 1
+          }}>
+          <Pressable style={styles.button} onPress={() => navigation.navigate('SignIn')}>
+            <Text style={styles.buttonText}> Login</Text>
+          </Pressable>
+
+          <Pressable style={styles.button} onPress={() => navigation.navigate('SignIn')}>
+            <Text style={styles.buttonText}> Sign Up</Text>
+          </Pressable>
+        </View>
+
+      </ImageBackground>
+    </View>
+  );
+}
+
+function SignIn() {
+  return (
+    <SafeAreaView>
+      <Text>Sign Up</Text>
+      <Text>Username</Text>
+      <TextInput style={styles.inputText} />
+      <Text>Password</Text>
+      <TextInput style={styles.inputText} />
+      <Text>(At least 8 characters)</Text>
+      <Image style={{ width: "40%", height: "10%" }} source={require("./assets/116d0e8f-2ca6-4088-ab0a-345e4c91aa4e.jpg")} />
       <Text>or continue with</Text>
       <View style={styles.iconView}>
         <Image
@@ -42,27 +106,25 @@ function LoginScreen() {
     </SafeAreaView>
   );
 }
-
-function signIn() {
-  return <Text>Sign Up</Text>;
-}
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
+      <Stack.Navigator initialRouteName="LoginScreen" screenOptions={{
+        headerShown: false
+      }}>
         <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Friends" component={Friends} options={{ name: "John" }} />
+        <Stack.Screen name="SignIn" component={SignIn} />
       </Stack.Navigator>
-    </NavigationContainer>
+    </NavigationContainer >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 200,
     flex: 1,
-    backgroundColor: "#fff",
     justifyContent: "center",
     alignItems: "center",
   },
@@ -71,6 +133,7 @@ const styles = StyleSheet.create({
     margin: 12,
     borderWidth: 1,
     padding: 10,
+    color: "black",
   },
   iconView: {
     padding: 20,
@@ -80,4 +143,29 @@ const styles = StyleSheet.create({
     width: "50%",
     height: "50%",
   },
-});
+  friendPic: {
+    width: 400,
+    height: 400,
+    borderRadius: 400 / 2
+  },
+  button: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    elevation: 8,
+    backgroundColor: "#E3967E",
+    borderRadius: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 12,
+    marginLeft: 70,
+    marginRight: 70,
+    marginTop: 15,
+    marginBottom: 15
+  },
+  buttonText: {
+    fontSize: 35,
+    color: "#fff",
+    fontWeight: "bold",
+    alignSelf: "center",
+    font: "Helvetica"
+  }
+})
